eeef4f6db47b3c16e2dcd30741acdc469267ec2a
danielmera2912 <92045080+danielmera2912@users.noreply.github.com>
2023-03-10 02:49:43 +0100

    fin de verdad

---
+++ src/main/java/com/example/crev_server_spring/controller/UsuarioEquipoController.java
@@ -0,0 +1,57 @@
+package com.example.crev_server_spring.controller;
+
+import com.example.crev_server_spring.error.UsuarioEquipoNotFoundException;
+import com.example.crev_server_spring.error.UsuarioNotFoundException;
+import com.example.crev_server_spring.modelo.Usuario;
+import com.example.crev_server_spring.modelo.UsuarioEquipo;
+import com.example.crev_server_spring.service.UsuarioEquipoService;
+import com.example.crev_server_spring.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class UsuarioEquipoController {
+    private final UsuarioEquipoService usuarioEquipoService;
+    @GetMapping("/usuario_equipo")
+    public List<UsuarioEquipo> obtenerTodos() {
+        List<UsuarioEquipo> result =  usuarioEquipoService.findAll();
+        if(result.isEmpty()){
+            throw new UsuarioEquipoNotFoundException();
+        }
+        return result;
+    }
+
+    @GetMapping("/usuario_equipo/{id}")
+    public UsuarioEquipo obtenerUno(@PathVariable Long id) {
+        return usuarioEquipoService.findById(id).orElseThrow(() -> new UsuarioEquipoNotFoundException(id));
+    }
+
+    @PostMapping("/usuario_equipo")
+    public UsuarioEquipo newUsuarioEquipo(@RequestBody UsuarioEquipo newUsuarioEquipo){
+        return usuarioEquipoService.save(newUsuarioEquipo);
+    }
+
+    @PutMapping("/usuario_equipo/{id}")
+    public UsuarioEquipo updateUsuario(@RequestBody UsuarioEquipo updateUsuario, @PathVariable Long id){
+        if (usuarioEquipoService.existsById(id)) {
+            updateUsuario.setId(id);
+            return usuarioEquipoService.save(updateUsuario);
+        } else {
+            throw new UsuarioEquipoNotFoundException(id);
+        }
+    }
+
+    @DeleteMapping("/usuario_equipo/{id}")
+    public UsuarioEquipo deleteUsuario(@PathVariable Long id) {
+        if(usuarioEquipoService.existsById(id)){
+            UsuarioEquipo result = usuarioEquipoService.findById(id).get();
+            usuarioEquipoService.deleteById(id);
+            return result;
+        }else{
+            throw new UsuarioEquipoNotFoundException(id);
+        }
+    }
+}
---
+++ src/main/java/com/example/crev_server_spring/controller/UsuarioEventoController.java
@@ -0,0 +1,57 @@
+package com.example.crev_server_spring.controller;
+
+import com.example.crev_server_spring.error.UsuarioEventoNotFoundException;
+import com.example.crev_server_spring.error.UsuarioNotFoundException;
+import com.example.crev_server_spring.modelo.Usuario;
+import com.example.crev_server_spring.modelo.UsuarioEvento;
+import com.example.crev_server_spring.service.UsuarioEventoService;
+import com.example.crev_server_spring.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class UsuarioEventoController {
+    private final UsuarioEventoService usuarioEventoService;
+    @GetMapping("/usuario_evento")
+    public List<UsuarioEvento> obtenerTodos() {
+        List<UsuarioEvento> result =  usuarioEventoService.findAll();
+        if(result.isEmpty()){
+            throw new UsuarioEventoNotFoundException();
+        }
+        return result;
+    }
+
+    @GetMapping("/usuario_evento/{id}")
+    public UsuarioEvento obtenerUno(@PathVariable Long id) {
+        return usuarioEventoService.findById(id).orElseThrow(() -> new UsuarioEventoNotFoundException(id));
+    }
+
+    @PostMapping("/usuario_evento")
+    public UsuarioEvento newUsuarioEvento(@RequestBody UsuarioEvento newUsuarioEvento){
+        return usuarioEventoService.save(newUsuarioEvento);
+    }
+
+    @PutMapping("/usuario_evento/{id}")
+    public UsuarioEvento updateUsuario(@RequestBody UsuarioEvento updateUsuarioEvento, @PathVariable Long id){
+        if (usuarioEventoService.existsById(id)) {
+            updateUsuarioEvento.setId(id);
+            return usuarioEventoService.save(updateUsuarioEvento);
+        } else {
+            throw new UsuarioEventoNotFoundException(id);
+        }
+    }
+
+    @DeleteMapping("/usuario_evento/{id}")
+    public UsuarioEvento deleteUsuario(@PathVariable Long id) {
+        if(usuarioEventoService.existsById(id)){
+            UsuarioEvento result = usuarioEventoService.findById(id).get();
+            usuarioEventoService.deleteById(id);
+            return result;
+        }else{
+            throw new UsuarioEventoNotFoundException(id);
+        }
+    }
+}
